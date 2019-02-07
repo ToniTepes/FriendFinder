db9996fc3316bda6324b3c08ddc2c36f5b9a88b2
Antonio Stacy Foushee <AntonioStacy@gmail.com>
2019-02-06 22:43:11 -0500

    first merge

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Jack',
+		photo: 'https://farm5.staticflickr.com/4527/38236235932_815da71e2f_b.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Elinore',
+		photo: 'https://farm5.staticflickr.com/4513/37491798354_b7ceabb355_b.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Bobbi',
+		photo: 'https://farm1.staticflickr.com/913/43154640272_a4466295d6_b.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Maja',
+		photo: 'https://farm5.staticflickr.com/4581/38336862141_6199db81bd_b.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Ruckus',
+		photo: 'https://farm5.staticflickr.com/4730/27514706809_032179f876_b.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Sammy',
+		photo: 'https://farm5.staticflickr.com/4613/24716821197_3a2fd2e5fe_z.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Leon',
+		photo: 'https://farm5.staticflickr.com/4605/39613925941_f746a6b8d4_b.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Jillian',
+		photo: 'https://farm5.staticflickr.com/4652/27828777979_05bc2019a3_b.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Lily',
+		photo: 'https://farm5.staticflickr.com/4555/24445726328_7195fa9b3f_b.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Roy',
+		photo: 'https://farm2.staticflickr.com/1802/43154525772_b885243ff8_b.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
